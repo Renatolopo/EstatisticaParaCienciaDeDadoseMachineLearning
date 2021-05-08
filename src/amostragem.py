e6b846bd9b985b81805e3636812f66d71fcbe574
Renatolopo <renatolopo62@gmail.com>
2021-05-08 10:30:43 -0300

    add amostragem estratificada e amostragem reservatorio

--- src/amostragem.py
+++ src/amostragem.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np 
 import pandas as pd 
+from sklearn.model_selection import StratifiedShuffleSplit
 
 def get_amostragem_aleatoria_simples(df, amostras):
     return df.sample(n = amostras)
@@ -32,9 +33,39 @@ def get_amostragem_agrupamento(df, amostras):
     grupo_selecionado = random.randint(0, numero_grupos)
     return df[df['grupo'] == grupo_selecionado]
 
+def get_amostragem_estratificada(df, amostras):
+    split = StratifiedShuffleSplit(test_size=(amostras/len(df)))
+    for x, y in split.split(df, df['c#default']):
+        df_x = df.iloc[x]
+        df_y = df.iloc[y]
+    
+    print(df_x.shape)
+    print(df_y.shape)
+    return df_y
+
+def get_amostragem_reservatorio(df, amostras):
+    stream = []
+    for i in range(len(df)):
+        stream.append(i)
+
+    i = 0
+    tamanho = len(df)
+    reservatorio = [0] * amostras 
+    for i in range(amostras):
+        reservatorio[i] = stream[i]
+
+    while i < tamanho:
+        j = random.randrange(i+1)
+        if j < amostras:
+            reservatorio[j] = stream[i]
+        i += 1
+    return  df.iloc[reservatorio]
+
 
 df = pd.read_csv('./data/credit_data.csv')
 df_amostragem_aleatoria_simples = get_amostragem_aleatoria_simples(df, 1000)
 df_amostragem_sistematica = get_amostragem_sistematica(df, 1000)
 df_amostragem_agrupamento = get_amostragem_agrupamento(df, 1000)
-print(df.shape) 
+df_amostragem_estratificada = get_amostragem_estratificada(df, 1000)
+df_amostragem_reservatrio = get_amostragem_reservatorio(df, 1000)
+print(df_amostragem_reservatrio) 
